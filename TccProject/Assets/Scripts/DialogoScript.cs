cdc44b775038aa4b48036bfaf160af9c7e9a93fe
zedando <percegonafelipe@gmail.com>
2025-06-17 17:19:15 -0300

    TCC

---
+++ TccProject/Assets/Scripts/DialogoScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(menuName ="ScriptableObject/Caracter")]
+public class DialogoScript : ScriptableObject
+{
+    // Start is called before the first frame update
+    
+    public string CharName;
+    public Sprite Sprite; 
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
